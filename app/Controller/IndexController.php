f044907c6ca449c639e25e25a703b308d78fff99
vagrant <vagrant@homestead>
2019-12-16 08:12:03 +0000

    init pro

---
+++ app/Controller/IndexController.php
@@ -0,0 +1,45 @@
+<?php
+
+declare(strict_types=1);
+/**
+ * This file is part of Hyperf.
+ *
+ * @link     https://www.hyperf.io
+ * @document https://doc.hyperf.io
+ * @contact  group@hyperf.io
+ * @license  https://github.com/hyperf-cloud/hyperf/blob/master/LICENSE
+ */
+
+namespace App\Controller;
+
+use Hyperf\HttpServer\Contract\RequestInterface;
+
+class IndexController extends AbstractController
+{
+    public function index(RequestInterface $request)
+    {
+        $user = $this->request->input('user', 'Hyperf');
+        $method = $this->request->getMethod();
+
+        return 6;
+    }
+
+    /**
+     * Notes: 异步上传图片
+     * User: mhl
+     * @param RequestInterface $request
+     * @return string
+     */
+    public function asyncUploadFile(RequestInterface $request)
+    {
+        $savePath = BASE_PATH . '/upload/images/';
+        if (!is_dir($savePath)) {
+            mkdir($savePath, 0777, true);
+        }
+        $fileName = rand(1000, 999) . '.' . $request->file('file')->getExtension();
+        $request->file('file')->moveTo($savePath . $fileName);
+        return 'ok';
+    }
+
+
+}
