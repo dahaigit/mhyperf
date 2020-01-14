910ee744dea81bb2ce8c9dc1bcdb25271d6889bd
menghailong <menghailong@xiyuemedia.com>
2020-01-14 10:10:48 +0800

    feat 新增jsonrpc-http服务

---
+++ app/JsonRpc/DahaiService.php
@@ -0,0 +1,44 @@
+<?php
+declare(strict_types=1);
+
+namespace App\JsonRpc;
+use Hyperf\RpcServer\Annotation\RpcService;
+
+/**
+ * 注意，如希望通过服务中心来管理服务，需在注解内增加 publishTo 属性
+ * @RpcService(name="DahaiService", protocol="jsonrpc-http", server="jsonrpc-http")
+ */
+class DahaiService implements CalculatorServiceInterface
+{
+    public function add($a, $b) {
+        // 如何请求
+        /*
+         * 请求地址：192.168.10.10:9502
+         * 请求内容：{
+                        "jsonrpc": "2.0",
+                        "method": "calculator/add",
+                        "params": [
+                            2,
+                            2
+                        ],
+                        "id": ""
+                    }
+         *
+         * */
+         return $a + $b;
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
